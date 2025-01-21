06e044b910fb99df07c948e04e52f31b513b80c9
Shadabbelim2 <shadabbelim008@gmail.com>
2025-01-21 16:04:29 +0530

    add user Authentication on website

--- vege/urls.py
+++ vege/urls.py
@@ -5,11 +5,13 @@ from vege import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.index , name= "home"),
-    path('show.html/' , views.show , name= "show"),
+    path('show' , views.show , name= "show"),
     path('delete-receipe/<id>/' , views.delete_receipe , name="delete_receipe"),
     path('update-receipe/<id>/' , views.update_receipe , name="update_receipe"),
     path('ragister' , views.ragister , name= "ragister"),
-    path('login' , views.login , name= "login"),
+    path('login-page' , views.login_page, name= "login_page"),
+    path('logout' , views.logout_page, name= "logout_page"),
+    
 
 
 
--- vege/views.py
+++ vege/views.py
@@ -2,8 +2,16 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from .models import Receipe  # Make sure your model name is correct
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate ,login , logout
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def index(request):
+    if request.user.is_anonymous:
+        return redirect("/login-page")
+   
     if request.method == "POST":
         receipe_name = request.POST.get('recipeName')  # Name should match your form field
         receipe_description = request.POST.get('recipeDescription')
@@ -16,7 +24,7 @@ def index(request):
                 receipe_description=receipe_description,
                 receipe_image=receipe_image
             )
-            return redirect('/show.html')
+            return redirect('show')
       
     return render(request, 'index.html')
 
@@ -31,7 +39,7 @@ def show(request):
 def delete_receipe(request , id):
     quereset=Receipe.objects.get(id=id)
     quereset.delete()
-    return redirect('/show.html')
+    return redirect('show')
 
 def update_receipe(request , id):
     queryset=Receipe.objects.get(id = id)
@@ -50,7 +58,45 @@ def update_receipe(request , id):
     return render(request , 'update.html' , context )
      
 def ragister(request):
+    if request.method =="POST":
+        username=request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        user = User.objects.filter(username = username)
+        if user.exists():
+            messages.error(request, "Username already Exists Plese Login in")
+
+            return redirect('ragister')
+
+        user=User(username=username , email = email )
+        user.save()
+        user.set_password(password)
+        user.save()
+        messages.success(request, "Ragister successfull Plese login.")
+
+        return redirect('ragister')
+
     return render(request , 'ragister.html')  
 
-def login(request):
-    return render(request , 'login.html')        
+def login_page(request):
+    if request.method =="POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print(username , password)
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, "User Not Ragister.")
+            return redirect('login_page')
+        user = authenticate(request, username=username , password=password)
+        if user is not None:
+             login(request, user)
+             return redirect('/') 
+            
+        else:
+            messages.error(request, "Invalid Crudntioals.")
+            return redirect('login_page')
+               
+    return render(request , 'login_page.html')      
+
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')  
